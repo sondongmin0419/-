16398b6d5cb9d4c120ae682da5b700d8d410e043
sondongmin0419 <dongmin.dev0419@gmail.com>
2024-04-25 23:09:09 +0900

    알고리즘 4/25

--- 2.스택_큐/주식가격.py
+++ 2.스택_큐/주식가격.py
@@ -1,3 +1,21 @@
 def solution(prices):
-    answer = []
+    answer = [0] * len(prices)
+    price_idx = []
+    for idx in range(len(prices)):
+        price_idx.append([prices[idx],idx])
+        
+        i = 0
+        while i < len(price_idx):
+            if price_idx[i][1] == idx:
+                i+=1
+                continue
+            if price_idx[i][0]<= prices[idx] :
+                answer[price_idx[i][1]] +=1
+                i+=1
+            else:
+                answer[price_idx[i][1]] +=1
+                price_idx.pop(i)
+    print(answer)
     return answer
+
+solution([1, 2, 3, 2, 3])
