6787b171c460ec5b6cfa1b9d12deb76be7d77be8
sondongmin0419 <dongmin.dev0419@gmail.com>
2024-05-20 21:30:32 +0900

    5/20 알고리즘

--- 4.정렬/가장큰수.py
+++ 4.정렬/가장큰수.py
@@ -1,3 +1,8 @@
 def solution(numbers):
     answer = ''
+    _numbers = list(map(str,numbers))
+    _numbers = sorted(_numbers, key=lambda x :x*3 , reverse=True)
+    answer = ''.join(_numbers)
     return answer
+
+solution([3, 30, 34, 5, 9]	)
