d8fd5e24fc7aec103bb4b301b87edb87de2c8474
sondongmin0419 <dongmin.dev0419@gmail.com>
2024-06-17 22:09:22 +0900

    알고리즘 6/17

--- 5.완전탐색/카펫.py
+++ 5.완전탐색/카펫.py
@@ -1,5 +1,22 @@
 def solution(brown, yellow):
     answer = []
+    x = 0
+    y = 0
+    # x * y = brown + yellow
+    # (x+y)*2 -4  = brown
+    # x + y = brown/2 +2
+    for i in range(3, int((brown/2 +2)/2) +1):
+        y = i
+        x = int(brown/2) + 2 - i
+        if x*y == brown+yellow:
+            answer = [x,y]
+            break
+    print(answer)
     
+            
+        
+        
     
     return answer
+
+solution(10,2)
