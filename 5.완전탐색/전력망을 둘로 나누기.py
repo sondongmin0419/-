9d083885740de81d2133e33efae3c088639c7cbb
sondongmin0419 <dongmin.dev0419@gmail.com>
2024-06-19 21:08:23 +0900

    알고리즘 6/19

---
+++ 5.완전탐색/전력망을 둘로 나누기.py
@@ -0,0 +1,13 @@
+def dfs():
+
+    return
+
+
+def solution(n, wires):
+    
+    answer = -1
+
+    return answer
+
+
+solution(9, [[1, 3], [2, 3], [3, 4], [4, 5], [4, 6], [4, 7], [7, 8], [7, 9]])
--- 5.완전탐색/피로도.py
+++ 5.완전탐색/피로도.py
@@ -1,6 +1,29 @@
 def solution(k, dungeons):
-    answer = -1
-    
-    return answer
-  
-solution(80, [[80,20],[50,40],[30,10]]	)
+    answer = [-1]
+    dungeons = sorted(dungeons, key=lambda x: [x[0], -x[1]], reverse=True)
+    a_k = k
+    dun_cnt = 0
+    len_dungeons = len(dungeons)
+    check_dungeons = [False] * len_dungeons
+    print(dungeons)
+
+    def roof(a_k, dun_cnt):
+        for i in range(len_dungeons):
+            if check_dungeons[i] == False and a_k >= dungeons[i][0] and a_k >= dungeons[i][1]:
+                check_dungeons[i] = True
+                a_k -= dungeons[i][1]
+                dun_cnt += 1
+                roof(a_k, dun_cnt)
+                a_k += dungeons[i][1]
+                check_dungeons[i] = False
+                dun_cnt -= 1
+        if answer[0] < dun_cnt:
+            answer[0] = dun_cnt
+        print(answer[0])
+
+    roof(a_k, dun_cnt)
+    print(answer[0])
+    return answer[0]
+
+
+solution(80, [[80, 20], [50, 40], [30, 10]])
